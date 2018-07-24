6255fd5672872f7d03422ecb1c75a023e7123606
Patoleme <hikari10151@hotmail.com>
2018-07-24 11:38:18 +0700

    Test Vertion 1

---
+++ Git/src/Git.java
@@ -0,0 +1,18 @@
+
+public class Git {
+	private int number;
+	
+	public Git() {
+		
+	}
+	public Git(int n) {
+		number=n;
+	}
+	public void setNumber(int n) {
+		number=n;
+	}
+	public int getNumber() {
+		return number;
+	}
+	
+}
---
+++ Git/src/Rtest.java
@@ -0,0 +1,10 @@
+
+public class Rtest {
+
+	public static void main(String[] args) {
+		Git a = new Git(5);
+		System.out.println("Number = "+a.getNumber());
+
+	}
+
+}
