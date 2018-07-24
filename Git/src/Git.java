ee91137cefd93a37536426c3782df86fe761a1c3
Patoleme <hikari10151@hotmail.com>
2018-07-24 11:54:09 +0700

    Vertion 2 aaa

--- Git/src/Git.java
+++ Git/src/Git.java
@@ -1,12 +1,13 @@
 
 public class Git {
-	private int number;
+	private int number,aaa;
 	
 	public Git() {
 		
 	}
-	public Git(int n) {
+	public Git(int n,int a) {
 		number=n;
+		aaa=a;
 	}
 	public void setNumber(int n) {
 		number=n;
@@ -14,5 +15,10 @@ public class Git {
 	public int getNumber() {
 		return number;
 	}
-	
+	public void setAaa(int n) {
+		aaa=n;
+	}
+	public int getAaa() {
+		return aaa;
+	}
 }
